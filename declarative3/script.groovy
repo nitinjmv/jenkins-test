0ab624fd64fbe728fbb5154998583ad824c0cd58
nitinjmv <nitinjmv@gmail.com>
2020-11-04 23:20:31 +0000

    trying out groovy
    variables defined in the jenkinsfile will be accessible here as well

---
+++ declarative3/script.groovy
@@ -0,0 +1,13 @@
+def buildApp(){
+  echo 'building app'
+}
+
+def testApp(){
+  echo 'testing app'
+}
+
+def deployApp(){
+  echo 'deploying app'
+  echo 'deploying version ${params.VERSION}'
+}
+return this
